d1d4fd10e394332e936a3a738e197369001d3147
Jack Mason <mrsideshowjack@gmail.com>
2017-02-15 16:08:46 +0000

    15 Feb

--- js/scroll.js
+++ js/scroll.js
@@ -26,3 +26,42 @@ var tweenhead = new TimelineMax()
 var scene1 = new ScrollMagic.Scene({duration: 700, offset: 150});
 scene1.setTween(tweenhead);
 scene1.addTo(controller);
+
+//animate images in problems/answers
+var tweenprob = new TimelineMax()
+	.to("#problem-img", 1, {opacity: 1, right: 0}, 0);
+var scene2 = new ScrollMagic.Scene({duration: 400, offset: -20});
+scene2.triggerElement("#problem");
+scene2.setTween(tweenprob);
+scene2.addTo(controller);
+
+var tweenanswr = new TimelineMax()
+	.to("#answer-img", 1, {opacity: 1, left: 0}, 0);
+var scene3 = new ScrollMagic.Scene({duration: 400, offset: -20});
+scene3.triggerElement("#answer");
+scene3.setTween(tweenanswr);
+scene3.addTo(controller);
+
+
+
+
+//
+
+var controller2 = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave'
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("div.panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i])
+				.addIndicators() // add indicators (requires plugin)
+				.addTo(controller2);
+		}
